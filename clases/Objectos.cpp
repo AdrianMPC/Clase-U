cfd0a6b6ec1e405604064c71d3476d235d069444
AdrianMPC <83367059+AdrianMPC@users.noreply.github.com>
2022-05-04 11:45:20 -0500

    Add files via upload

---
+++ clases/Objectos.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "alumno.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//constructor
+	//crear objecto1 
+	Alumno objAlumno1 ("C001","Daniel Pero",20,15);
+	//imprimir
+	cout<<"Nombre: " <<objAlumno1.getNomApe()<<"\t";
+	cout<<"Codigo: " <<objAlumno1.getCodigo()<<"\t";
+	cout<<"Nombre: " <<objAlumno1.getPc1()<<"\t";
+	cout<<"Nombre: " <<objAlumno1.getPc2()<<"\t";
+	cout<<"Promedio alumno1: "<<objAlumno1.getPromedio()<<"\n";
+	//crear objecto2
+	Alumno objAlumno2 ( "C002","Daniel Díaz",19,14 );
+	//imprimir
+	cout<<"Nombre: " <<objAlumno2.getNomApe()<<"\t";
+	cout<<"Codigo: " <<objAlumno2.getCodigo()<<"\t";
+	cout<<"Nombre: " <<objAlumno2.getPc1()<<"\t";
+	cout<<"Nombre: " <<objAlumno2.getPc2()<<"\t";
+	cout<<"Promedio alumno2: "<<objAlumno2.getPromedio()<<"\n";
+	
+	return 0;
+}
+
---
+++ clases/alumno.h
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Alumno{
+	private:
+		string codigo;
+		string nombre;
+		float pc1;
+		float pc2;
+	public:
+		//constructor 
+		Alumno(string, string, float, float);
+		//set
+		void setCodigo(string cod);
+		void setNomApe(string nApe);
+		void setN1(float n1);
+		void setN2(float n2);
+		//get
+		string getCodigo();
+		string getNomApe();
+		float getPc1();
+		float getPc2();
+		//operacion
+		float getPromedio();
+};
+//constructor implementacion
+Alumno::Alumno(string cod, string nApe, float n1, float n2)
+{
+	this->codigo = cod;
+	this->nombre = nApe;
+	pc1 = n1;
+	pc2 = n2;
+}
+
+//set
+void Alumno::setCodigo(string cod)
+{
+	this->codigo = cod;
+}
+void Alumno::setNomApe(string nApe)
+{
+	this->nombre = nApe;
+}
+void Alumno::setN1(float n1)
+{
+	pc1 = n1;
+}
+void Alumno::setN2(float n2)
+{
+	pc2 = n2;
+}
+
+
+//get
+string Alumno::getNomApe()
+{
+	return this->nombre;
+}
+string Alumno::getCodigo()
+{
+	return this->codigo;
+}
+float Alumno::getPc1()
+{
+	return this-> pc1;
+}
+float Alumno::getPc2()
+{
+	return this-> pc2;
+}
+
+
+
+//operacion
+float Alumno::getPromedio()
+{
+	return (this->pc1 + this->pc2)/2;
+}
