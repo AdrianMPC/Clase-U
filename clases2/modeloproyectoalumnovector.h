750819f33aed124721c68331a6e6a0fcc3493184
AdrianMPC <83367059+AdrianMPC@users.noreply.github.com>
2022-05-09 12:43:35 -0500

    Add files via upload

---
+++ clases2/modeloproyectoalumnovector.h
@@ -0,0 +1,49 @@
+#include<vector>
+#include"modeloproyectoalumno.h"
+#include<iostream>
+using namespace std;
+class AlumnoVector
+{
+private:
+	vector<Alumno> vectorAlumno;
+public:
+	AlumnoVector()
+	{
+		vector<Alumno> vectorAlumno;
+	}
+	void add(Alumno obj) /*Agrega objectos al vector uwu*/
+	{
+		vectorAlumno.push_back(obj);
+	}
+	Alumno get(int pos)/*Entrega el objecto en la pos actual de la memoria*/
+	{
+		return vectorAlumno[pos];
+	}
+	int rows()/*Tamaño actual del vector*/
+	{
+		return vectorAlumno.size();
+	}
+	Alumno buscarPorCodigo(int codigo)
+	{
+		for (Alumno x : vectorAlumno)
+		{
+			if (codigo == x.getCodigo())
+			{
+				return x;
+			}
+		}
+	}
+	int getPostArray(Alumno obj)
+	{
+		for (unsigned int i = 0; i < vectorAlumno.size(); i++)
+		{
+			if (obj.getCodigo() == 
+				vectorAlumno[i].getCodigo())
+			{
+				return i;/*Retorna la posicion del 
+				elemento dentro del vector*/
+			}
+		}
+	}
+	
+};
